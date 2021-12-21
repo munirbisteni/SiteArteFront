d598a56ea7a67f3874abf119eae02cad53fc1eee
Munirr00 <munir.mlcb@gmail.com>
2021-12-21 10:56:16 -0300

    isabelafront

--- request/request.js
+++ request/request.js
@@ -1,18 +1,18 @@
 function ShowOpcaoLivro(){
-    var livro = document.querySelector('#opcaoLivro');
-    livro.style.display = 'flex';
-    var tela = document.querySelector('#opcaoTela');
+    var livro = document.querySelector('#opcao-livro');
+    livro.style.display = 'block';
+    var tela = document.querySelector('#opcao-tela');
     tela.style.display = 'none'; 
 }
 function ShowOpcaoTela(){
-    var tela = document.querySelector('#opcaoTela');
+    var tela = document.querySelector('#opcao-tela');
     tela.style.display = 'flex'; 
-    var livro = document.querySelector('#opcaoLivro');
+    var livro = document.querySelector('#opcao-livro');
     livro.style.display = 'none';
 }
 function ShowOpcaoNenhuma(){
-    var tela = document.querySelector('#opcaoTela');
+    var tela = document.querySelector('#opcao-tela');
     tela.style.display = 'none'; 
-    var livro = document.querySelector('#opcaoLivro');
+    var livro = document.querySelector('#opcao-livro');
     livro.style.display = 'none';
 }
