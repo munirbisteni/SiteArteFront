778786e4d9bc3c3bdb1464820f976d6d0a52116f
Munirr00 <munir.mlcb@gmail.com>
2022-01-14 00:12:27 -0300

    atualizacao

--- request/request.js
+++ request/request.js
@@ -3,14 +3,29 @@ function ShowOpcaoLivro(){
     livro.style.display = 'block';
     var tela = document.querySelector('#opcao-tela');
     tela.style.display = 'none'; 
+    var eco = document.querySelector('#opcao-eco');
+    eco.style.display = 'none'; 
 }
 function ShowOpcaoTela(){
     var tela = document.querySelector('#opcao-tela');
     tela.style.display = 'flex'; 
     var livro = document.querySelector('#opcao-livro');
     livro.style.display = 'none';
+    var eco = document.querySelector('#opcao-eco');
+    eco.style.display = 'none'; 
 }
 function ShowOpcaoNenhuma(){
+    var tela = document.querySelector('#opcao-tela');
+    tela.style.display = 'none'; 
+    var livro = document.querySelector('#opcao-livro');
+    livro.style.display = 'none';
+    var eco = document.querySelector('#opcao-eco');
+    eco.style.display = 'none'; 
+}
+
+function ShowOpcaoEco(){
+    var eco = document.querySelector('#opcao-eco');
+    eco.style.display = 'flex'; 
     var tela = document.querySelector('#opcao-tela');
     tela.style.display = 'none'; 
     var livro = document.querySelector('#opcao-livro');
